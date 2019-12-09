3cefc390872b38cabbcf564ad0bf6c6bb560086d
fan <f523_521@163.com>
2019-12-09 16:57:59 +0800

    好好

--- src/main/java/com/example/hospital_backstage/pojo/GV.java
+++ src/main/java/com/example/hospital_backstage/pojo/GV.java
@@ -2,8 +2,8 @@ package com.example.hospital_backstage.pojo;
 
 /**
  * @author Administrator
- * @create 2019-12-09 16:16
+ * @create 2019-12-09 16:57
  * @desc
  **/
-public class HelloWord {
+public class GV {
 }
